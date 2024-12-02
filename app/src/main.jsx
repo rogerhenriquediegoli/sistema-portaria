f8227fecb20007d59321c90cd171fc1c5e1462fb
Roger Henrique Diegoli <rogerhenriquediegoli@gmail.com>
2024-12-02 02:51:07 -0300

    update

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={window.location.pathname || ''}>  {/* Definindo o basename */}
+    <BrowserRouter basename={window.location.pathname || '/'}>  {/* Definindo o basename */}
       <App />
     </BrowserRouter>
   </StrictMode>,
